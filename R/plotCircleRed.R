1ebad4eacb23d2cfa51249a5c942a4b0efb3d87a
Linbo Wang <lbwang@uw.edu>
2015-07-20 23:08:29 -0700

    liteep's advice

---
+++ R/plotCircleRed.R
@@ -0,0 +1,23 @@
+#' Plot a square of side length r.
+#'
+#' Normally, we would now describe what
+#' the function is supposed to do
+#'
+#' @param r The side length of the square to
+#'   be plotted
+#'
+#' @return None The function is called
+#'   solely for its side effect
+#' @examples
+#' plotSquare(5)
+#'
+#' @seealso The general
+#'   \code{\link[graphics]{plot}} function
+#'
+#' @export
+plotSquareRed <- function(r=1){
+  myAngles <- seq(from=0, to=2*pi,length.out=200)
+  x <- cos(myAngles)
+  y <- sin(myAngles)
+  plot(r*x, r*y, type="l", main=paste0("A Circle of Radius ", r),col="red")
+}
